947854e2c8bbffa17edbdc6ea8bba209ef7b95b2
ihorshnaider <ihor_shnaider@outlook.com>
2024-06-22 23:03:54 +0300

    .

--- infra/aws/terraform/variables.tf
+++ infra/aws/terraform/variables.tf
@@ -3,11 +3,11 @@ variable "env" {
   type = string
 }
 
-variable "aws_region" {
-  default     = "us-east-1"
-  description = "aws region"
-  type = string
-}
+# variable "aws_region" {
+#   default     = "us-east-1"
+#   description = "aws region"
+#   type = string
+# }
 
 variable "app_name" {
   description = "application name"
