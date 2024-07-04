fbabe87fcbdae62ed30afd66674d9baa55958be4
ihorshnaider <ihor_shnaider@outlook.com>
2024-07-04 14:08:47 +0300

    Add clean up

--- infra/aws/scripts/clean-up.sh
+++ infra/aws/scripts/clean-up.sh
@@ -1,7 +1,7 @@
 APP_NAME=$1
 ENV=$2
 
-./create-backend-conf.sh $APP_NAME $ENV #us-east-1
+./create-backend-conf.sh $APP_NAME $ENV
 
 terraform -chdir=../terraform init  -backend-config=backend.conf
 
@@ -14,3 +14,21 @@ aws s3api delete-objects \
       jq '{Objects: [.Versions[] | {Key:.Key, VersionId : .VersionId}], Quiet: false}')"
 
 aws dynamodb delete-table --table-name terraform-locks-$APP_NAME
+
+#Delete user
+USER_NAME="github-actions-user-$APP_NAME"
+POLICIES=$(aws iam list-attached-user-policies --user-name $USER_NAME --query "AttachedPolicies[*].PolicyArn" --output text)
+
+for POLICY_ARN in $POLICIES
+do
+  aws iam detach-user-policy --user-name $USER_NAME --policy-arn $POLICY_ARN
+done
+
+ACCESS_KEYS=$(aws iam list-access-keys --user-name $USER_NAME --query "AccessKeyMetadata[*].AccessKeyId" --output text)
+
+for ACCESS_KEY_ID in $ACCESS_KEYS
+do
+  aws iam delete-access-key --user-name $USER_NAME --access-key-id $ACCESS_KEY_ID
+done
+
+aws iam delete-user --user-name $USER_NAME
